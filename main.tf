722cf0ecd694507a7d02f653a2284064748125ea
danushvithiyarth <158765030+danushvithiyarth@users.noreply.github.com>
2025-09-22 15:45:06 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-075449515af5df0d1"
   instance_type = "t3.micro"
-  key_name      = "keyInterminal"
+  key_name      = "keyInTerminal"
   tags = {
     Name = "terraform machine2"
   }
